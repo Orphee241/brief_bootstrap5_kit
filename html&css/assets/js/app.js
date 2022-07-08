a4d3d263ae8fa59a715c8f281668c9d52feec012
Orphee241 <nzienguiakoumbou@gmail.com>
2022-07-08 18:02:48 +0100

    Cinquième commit

--- html&css/assets/js/app.js
+++ html&css/assets/js/app.js
@@ -1,2 +1,2 @@
-gsap.from(".steeven", {y: -80, duration: 1, delay:1, })
-gsap.from(".creativeDirector", {y: -25, duration: 1, delay:2})
+gsap.from(".steeven", {y: -150, duration: 1, delay:1, });
+gsap.from(".creativeDirector", {y: -25, duration: 1, delay:2});
